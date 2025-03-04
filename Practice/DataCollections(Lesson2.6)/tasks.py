f57bcb68a0f8b8190b2ee976c6b151c871b53425
Sanjar <sanjarkojobekov@gmail.com>
2025-03-05 02:48:18 +0600

    Урок 2.6

---
+++ Practice/DataCollections(Lesson2.6)/tasks.py
@@ -0,0 +1,19 @@
+list = ["kotok", "am", "tashak"]
+
+def addElementToList(str):
+    list.append(str)
+    return print(list)
+addElementToList(str(input("Введите ваше слово: ")))
+
+def deleteElementFromList(int):
+    list.remove(list[int])
+    return print(list)
+deleteElementFromList(1)
+
+cities = ["Moscow", "Saint-Petersburg", "New York", "Tokyo"]
+
+def pickCityByIndex(int):
+    return print(cities[int])
+pickCityByIndex(1)
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
